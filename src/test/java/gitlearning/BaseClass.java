ca9acbb3ffd4283b7d826936b3aca7efb31ac237
anwer <anwersahira786@gmail.com>
2025-04-24 21:03:58 +0530

    fixup! Impl the bass class browser with setup and tear down

--- src/test/java/gitlearning/BaseClass.java
+++ src/test/java/gitlearning/BaseClass.java
@@ -1,7 +1,6 @@
 package gitlearning;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
