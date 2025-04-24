edd241c0fccae7522d72e45191d973133e8545f6
anwer <anwersahira786@gmail.com>
2025-04-24 23:32:52 +0530

    Code refraction

--- src/test/java/gitlearning/BaseClass.java
+++ src/test/java/gitlearning/BaseClass.java
@@ -1,25 +1,22 @@
 package gitlearning;
 
+import gitpractice.browser.BrowserManger;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-
-import java.time.Duration;
 
 public class BaseClass {
 
     protected WebDriver driver = null;
+    private final static String BROWSER = "firefox";
 
     @BeforeMethod
     public void setup() {
-        driver = new FirefoxDriver();
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver = BrowserManger.initBrowser(BROWSER);
     }
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();
+        BrowserManger.tearDownBrowser();
     }
 }
